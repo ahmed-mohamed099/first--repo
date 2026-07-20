b12be15e6443349667d978dd6b88f16619b897d1
ahmed-mohamed099 <ahmed.mohamed.03@icloud.com>
2026-07-20 04:54:15 +0200

    add my name

---
+++ my_file.dart
@@ -0,0 +1,17 @@
+void main () {
+  int x = 14 , y= 12 ;
+  if(x>y){
+    print("x is greater than y");
+  }else if (y>x) {
+ print  ("y is greater than x " ) ;
+   } else {
+    print ("Are equal") ;
+    
+
+
+  }
+
+
+  }
+
+
